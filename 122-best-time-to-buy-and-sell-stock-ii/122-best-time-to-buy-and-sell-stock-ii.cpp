3035a5623d61fe4fec7a87c9682fd47f5b2e3b81
Harshit254 <61619762+Harshit254@users.noreply.github.com>
2022-09-10 16:41:35 +0530

    Time: 32 ms (5.28%), Space: 17.3 MB (10.12%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,29 @@
 class Solution {
+// public:
+//     int maxProfit(vector<int>&nums) {
+//         int sum=0;
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             if(nums[i]>nums[i-1])
+//             {
+//                 sum+=nums[i]-nums[i-1];
+//             }
+//         }
+//         return sum;
+//     }
+    
+     int maxProfit(vector<int> &pr, int ind, int buy, vector<vector<int>> &dp) {
+        if(ind >= pr.size())return 0;
+        if(dp[ind][buy]!=-1)return dp[ind][buy];
+        
+        if(buy)
+            return dp[ind][buy] = max(-pr[ind] + maxProfit(pr, ind + 1, 0, dp), maxProfit(pr, ind + 1, 1, dp));
+        else
+            return dp[ind][buy] = max(pr[ind] + maxProfit(pr, ind + 1, 1, dp), maxProfit(pr, ind + 1, 0, dp));
+    }
 public:
-    int maxProfit(vector<int>&nums) {
-        int sum=0;
-        for(int i=1;i<nums.size();i++)
-        {
-            if(nums[i]>nums[i-1])
-            {
-                sum+=nums[i]-nums[i-1];
-            }
-        }
-        return sum;
+    int maxProfit(vector<int>& pr) {
+        vector<vector<int>> dp(pr.size() + 1, vector<int>(2, -1));
+        return maxProfit(pr, 0, 1, dp);
     }
 };
