9a7c60ac559fcfc75f2522d44cb4d69a8a683026
Harshit254 <61619762+Harshit254@users.noreply.github.com>
2022-10-07 18:27:17 +0530

    Time: 56 ms (20.78%), Space: 14.5 MB (12.32%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
+int fun(int i,int n,vector<int>&prices,int buy,int trans,vector<vector<vector<int>>>&dp)
+{
+    if(i==n || trans==0) return 0;
+    if(dp[i][buy][trans]!=-1) return dp[i][buy][trans];
+    
+    if(buy) return dp[i][buy][trans]=max(-prices[i]+fun(i+1,n,prices,0,trans,dp),0+fun(i+1,n,prices,1,trans,dp));
+    else return dp[i][buy][trans]=max(prices[i]+fun(i+1,n,prices,1,trans-1,dp),0+fun(i+1,n,prices,0,trans,dp));
+}
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> ahead(2,vector<int> (k+1,0));
-        vector<vector<int>> cur(2,vector<int> (k+1,0));
-        for(int ind = n-1; ind>=0; ind--){
-            for(int buy = 0; buy<=1; buy++){
-                for(int cap=1; cap<=k; cap++){
-                    if(buy==0){// We can buy the stock
-                        cur[buy][cap] = max(0+ahead[0][cap],-prices[ind] + ahead[1][cap]);
-                    }
-                    if(buy==1){// We can sell the stock
-                        cur[buy][cap] = max(0+ahead[1][cap],prices[ind] + ahead[0][cap-1]);
-                    }
-                }
-            }
-            ahead = cur;
-        }
-        return ahead[0][k];
+        // int n = prices.size();
+        // vector<vector<int>> ahead(2,vector<int> (k+1,0));
+        // vector<vector<int>> cur(2,vector<int> (k+1,0));
+        // for(int ind = n-1; ind>=0; ind--){
+        //     for(int buy = 0; buy<=1; buy++){
+        //         for(int cap=1; cap<=k; cap++){
+        //             if(buy==0){// We can buy the stock
+        //                 cur[buy][cap] = max(0+ahead[0][cap],-prices[ind] + ahead[1][cap]);
+        //             }
+        //             if(buy==1){// We can sell the stock
+        //                 cur[buy][cap] = max(0+ahead[1][cap],prices[ind] + ahead[0][cap-1]);
+        //             }
+        //         }
+        //     }
+        //     ahead = cur;
+        // }
+        // return ahead[0][k];
+        int n=prices.size();
+    vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+    return fun(0,n,prices,1,k,dp);
     }
 };
