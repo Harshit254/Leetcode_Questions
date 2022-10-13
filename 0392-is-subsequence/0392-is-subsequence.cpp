1a16e3e1a965e7b29a6ed7fac4f5c3f554a26f32
Harshit254 <61619762+Harshit254@users.noreply.github.com>
2022-10-13 20:02:46 +0530

    Time: 2 ms (61.46%), Space: 6.2 MB (88.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -10,25 +10,29 @@ public:
             return dp[i][j]=max(fun(i-1,j,s,t,dp),fun(i,j-1,s,t,dp));
         }
     }
-    bool isSubsequence(string s, string t) {
-        vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,0));
-        int n=s.size();
-        int m=t.size();
-        //return s.size()==fun(s.size(),t.size(),s,t,dp);
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s[i-1]==t[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return s.size()==dp[n][m];
+    // bool isSubsequence(string s, string t) {
+    //     vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,0));
+    //     int n=s.size();
+    //     int m=t.size();
+    //     //return s.size()==fun(s.size(),t.size(),s,t,dp);
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         for(int j=1;j<=m;j++)
+    //         {
+    //             if(s[i-1]==t[j-1])
+    //             {
+    //                 dp[i][j]=1+dp[i-1][j-1];
+    //             }
+    //             else
+    //             {
+    //                 dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    //             }
+    //         }
+    //     }
+    //     return s.size()==dp[n][m];
+    // }
+    bool isSubsequence(string s, string t, int i = 0) {
+        for(int j = 0; j < t.length(); j++) if(s[i] == t[j])i++;
+        return i == s.length();
     }
 };
